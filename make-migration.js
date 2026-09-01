a0a3201c3129c5021dce8d138731f7a1aa8e6d3b
RS_ict <r.stefani87.informatica@gmail.com>
2026-09-01 19:13:52 +0200

    chore: add migration scripts

---
+++ make-migration.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+import { spawn } from "node:child_process";
+import { fileURLToPath } from "node:url";
+import { dirname, join } from "node:path";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const cliEntry = join(__dirname, "index.js");
+const argv = process.argv.slice(2);
+const node = process.execPath;
+
+const args = [cliEntry, "migration", "make", ...argv];
+
+const child = spawn(node, args, { stdio: "inherit" });
+child.on("error", (e) => {
+  console.error("[make-migration] wrapper failed", e);
+  process.exit(1);
+});
+child.on("close", (code) => process.exit(code ?? 0));
